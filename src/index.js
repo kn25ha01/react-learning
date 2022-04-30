cf8e5f01b1e260c90d47ed1bf1c2b47dda49b488
kn25ha01 <kn25ha01@gmail.com>
2022-04-30 10:31:34 +0900

    play tutorial 3

--- src/index.js
+++ src/index.js
@@ -20,26 +20,30 @@ class Board extends React.Component {
     );
   }
 
-  render() {
+  renderBoard(n) {
+    const board = [];
+    for (let i = 0; i < n; i++) {
+      const row = [];
+      for (let j = 0; j < n; j++) {
+        row.push(
+          this.renderSquare(i * n + j)
+        )
+      }
+      board.push(
+        <div className="board-row">
+          {row}
+        </div>
+      );
+    };
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
+  }
+
+  render() {
+    return this.renderBoard(3);
   }
 }
 
